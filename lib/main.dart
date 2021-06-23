26527de5ccac5f68b29395cf7324194351ab9efd
Shoma <sho1228.k@gmail.com>
2021-06-24 00:28:03 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: (
+      theme: ThemeData(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'hoge'),
     );
   }
 }
